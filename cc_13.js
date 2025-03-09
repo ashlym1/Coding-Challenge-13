74a670c0aca0b43563bf53def64a62059aa6e2d2
ashlym1 <ashlym3@usf.edu>
2025-03-09 13:23:44 -0400

    Tasl 3- Bulk update on employee card

--- cc_13.js
+++ cc_13.js
@@ -1,7 +1,7 @@
 
   // Selecting the container for the employee cards 
  const employeeContainer = document.getElementById("employeeContainer");
-// Task 2- Function to add employee cards dynamically 
+// Task 2- Adding  Employee cards dynamically 
 function addEmployee (name,position) {
   const card = document.createElement ("div") ; 
   card.className= "employee-card" ;  
@@ -27,4 +27,13 @@ function addEmployee (name,position) {
 // Test Case : 
 addEmployee("Cindy Lu ", "Marketing Intern ");
 addEmployee("Snoopy joe ", "Marketing Manager");
-
+ 
+// Task 3- Converting NodeList to Arrays for bulk updates :
+function highlightEmployees() {
+    let employeed= document.querySelectorAll(".employee-card");// this selectes all the employee cards
+    let employeesArray=Array.from (employees);// converting  the node list to an array 
+     employeesArray.foreach (card => {
+        card.classList.add("highlight"); //  Highlighting class 
+     });
+     }
+ // Test case : 
